a3345ed3f65259bf15026ca5f726357c6a2ee8a4
NaHwangje <na03266@naver.com>
2023-08-18 15:55:51 +0900

    설정 파일 추가, map형식 뷰로 출력

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,17 +1,32 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.CommentJdbcDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class MainController {
 
+    @Autowired
+    private CommentJdbcDAO commentJdbcDAO;
+
     @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
-        model.addAttribute("message", "안녕 나는 자바야");
-        model.addAttribute("name", name);
+
+        List<Map<String, ?>> commentList = commentJdbcDAO.selectAllCommentList();
+        Map<String, ?> cmt = commentList.get(0);
+
+
+        model.addAttribute("no", cmt.get("no"));
+        model.addAttribute("author", cmt.get("author"));
+        model.addAttribute("comment", cmt.get("comment"));
+
         return "main";
     }
 }
