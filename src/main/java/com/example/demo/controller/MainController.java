106519aded32907750d86f863a6ee3110c825d5d
NaHwangje <na03266@naver.com>
2023-08-18 11:52:03 +0900

    모델 임포트 방법 확인

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 @RestController
 public class MainController {
 
     @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+        model.addAttribute("message", "안녕 나는 자바야");
         return "main";
     }
 }
