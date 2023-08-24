aeff44fe9cd06bba232dd41cede5b5a22c99941c
NaHwangje <na03266@naver.com>
2023-08-24 16:17:59 +0900

    링크 오류 해결 컨트롤러와 레스트 컨트롤러 차이 확인

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,14 +3,16 @@ package com.example.demo.controller;
 import com.example.demo.dao.CommentDAO;
 import com.example.demo.model.CommentModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@RestController
+/*
+    restcontroller로 되어있으면 리턴값이 자동으로 출력되게 된다. 그래서 일반 컨트롤러로 바꿔줘야한다.
+ */
+@Controller
 public class MainController {
 
     @Autowired
@@ -20,7 +22,7 @@ public class MainController {
         메인 페이지 - 댓글 목록 표시
      */
     @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+    public String hello(@RequestParam(value = "name", defaultValue = "") String name, Model model) {
 
         List<CommentModel> cmList = commentDAO.selectAllCommentList();
 
