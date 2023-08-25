e202c28d86c619fdd5d32bc08b9799ff5b38b871
NaHwangje <na03266@naver.com>
2023-08-25 14:49:12 +0900

    json애너테이션 주석 추가

--- src/main/java/com/example/demo/model/CommentModel.java
+++ src/main/java/com/example/demo/model/CommentModel.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
@@ -11,5 +12,6 @@ public class CommentModel {
     @JsonIgnore //리스폰스 바디로 호출했을때 api가 내려가면 안되는건 이렇게 씀
     private String author;
     private String comment;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm") // 시간의 자료형을 수정해서 전송
     private LocalDateTime date;
 }
